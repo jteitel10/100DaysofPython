8d7f9b4bae22f01fab4361cddfb6361ee8b5ef7d
Javier <46163181+jteitel10@users.noreply.github.com>
2021-02-03 10:58:47 -0500

    Add files via upload
    Day 16 - Reviewed the Turtle Module documentation.  Additionally, reviewed the PrettyTable module and created a sample Pokemon table based on the original three starters.

---
+++ 10s/16/main.py
@@ -0,0 +1,22 @@
+# import turtle
+
+# timmy = turtle.Turtle()# timmy.color("coral")
+# timmy.forward(100)
+
+# my_screen = turtle.Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.field_names = ["Pokemon Name", "Type"]
+table.add_rows(
+    [
+    ["Pikachu", "Electric"],
+    ["Squirtle", "Water"],
+    ["Charmander", "Fire"]
+    ]
+)
+table.align = 'l'
+print(table)
