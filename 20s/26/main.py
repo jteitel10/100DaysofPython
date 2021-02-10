cc8a10f6591a9db463eef9e4538273b3ded26d97
Javier <46163181+jteitel10@users.noreply.github.com>
2021-02-10 13:54:40 -0500

    Add files via upload
    Day 30 - Updated the program to handle errors driven by inputting characters not supported by the dictionary (i.e. : numbers and other characters).

--- 20s/26/main.py
+++ 20s/26/main.py
@@ -3,6 +3,15 @@ import pandas
 data = pandas.read_csv('nato_phonetic_alphabet.csv')
 nato_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
-user_input = input("Enter a word:\n").upper()
-phonetic_list = [nato_dict[letter] for letter in user_input]
-print(phonetic_list)
+def phonetic_generate():
+    user_input = input("Enter a word:\n").upper()
+    try:
+        phonetic_list = [nato_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        phonetic_generate()
+    else:
+        print(phonetic_list)
+
+
+phonetic_generate()
