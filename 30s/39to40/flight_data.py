6e45dfcb67c51b413d600824df8e48f1cec46049
Javier <46163181+jteitel10@users.noreply.github.com>
2021-02-25 10:06:13 -0500

    Update flight_data.py

--- 30s/39to40/flight_data.py
+++ 30s/39to40/flight_data.py
@@ -1,5 +1,5 @@
 class FlightData:
-    def __init_(self, price, origin_city, origin_airport, departure_airport_code, departure_city, out_date, return_date):
+    def __init__(self, price, origin_city, origin_airport, departure_airport_code, departure_city, out_date, return_date):
         self.price = price
         self.origin_city = origin_city
         self.origin_airport = origin_airport
